840e3dab45daf9f363faae1a39980e6642b54f68
binitbanerjee <binitbanerjee@gmail.com>
2018-10-11 18:44:57 -0400

    Updating PeerInfo

--- src/Node.java
+++ src/Node.java
@@ -1,8 +1,51 @@
 import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.File;
 import java.io.InputStreamReader;
 import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+
+class PeerInfo{
+    private int _columnId;
+    public int getColumnId(){
+        return _columnId;
+    }
+
+    private String _hostName;
+    public int getHostName(){
+        return _columnId;
+    }
+
+    private int _portNumber;
+    public int getPortNumber(){
+        return _columnId;
+    }
+
+    private boolean _hasFile;
+    public int getHasFile(){
+        return _columnId;
+    }
+
+    public PeerInfo(){
+        String thisLine = null;
+        File file = new File("PeerInfo.cfg");
+        try {
+            FileReader fReader = new FileReader(file);
+            BufferedReader breader = new BufferedReader(new FileReader(file));
+            while ((thisLine = breader.readLine()) != null) {
+                System.out.println(thisLine);
+            }
+        }
+        catch (Exception ex)
+        {
+
+        }
+    }
+}
+
+
+
 public class Node {
     private ServerSocket server;
     public Node(String ipAddress) throws Exception {
