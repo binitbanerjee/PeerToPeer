ed4fb14e5d4ed606ab3d06bc187241558c56b904
binitbanerjee <binitbanerjee@gmail.com>
2018-10-12 20:05:37 -0400

    Splitting code and allowing server and client to start together

--- src/Logger.java
+++ src/Logger.java
@@ -1,1 +1,52 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.PrintWriter;
+import java.util.Calendar;
 
+public class Logger{
+    private static final String logFilePath= "/home/dell/Documents/MS/CN/Project/BitTorrentP/src/PeerLog.txt";
+    PrintWriter printWriter;
+    private static File file;
+    //use it when running from terminal
+    //private static final String logFilePath= "/home/dell/Documents/MS/CN/Project/BitTorrentP/src/PeerLog.txt";
+    public Logger(){
+        file = new File(logFilePath);
+        try {
+            if (file.createNewFile()) {
+                printWriter = new PrintWriter(logFilePath);
+
+                printWriter.println("Log creation started at ");
+                printWriter.close();
+            }
+        }
+        catch (Exception ex)
+        {
+            System.out.println(ex.getMessage());
+        }
+    }
+
+    public void Message(String message)
+    {
+        if(file.exists())
+        {
+            try {
+                File file = new File(logFilePath).getAbsoluteFile();
+                FileReader fReader = new FileReader(file);
+                BufferedReader breader = new BufferedReader(new FileReader(file));
+                String thisLine = "";
+                String data = "";
+                while ((thisLine = breader.readLine()) != null) {
+                    data +=thisLine +"\n";
+                }
+                Calendar dt = Calendar.getInstance();
+                printWriter = new PrintWriter(logFilePath);
+                printWriter.println(data + dt.getTime() +" -- "+ message);
+                printWriter.close();
+            }
+            catch (Exception ex){
+                System.out.println("Something wrong with Logger");
+            }
+        }
+    }
+}
--- src/PeerInfo.java
+++ src/PeerInfo.java
@@ -1,1 +1,64 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class PeerInfo{
+    private Logger logger = new Logger();
+    private static final String filePath = "/home/dell/Documents/MS/CN/Project/BitTorrentP/src/PeerInfo.cfg";
+    private int _peerId;
+    public int getColumnId(){
+        return _peerId;
+    }
+
+    private String _hostName;
+    public String getHostName(){
+        return _hostName;
+    }
+
+    private int _portNumber;
+    public int getPortNumber(){
+        return _portNumber;
+    }
+
+    private boolean _hasFile;
+    public boolean getHasFile(){
+        return _hasFile;
+    }
+    List peers = new ArrayList();
+
+
+    public PeerInfo(){
+
+
+    }
+    public void init()
+    {
+        String thisLine = null;
+        //to be used while running from terminal
+        //String filePath = "PeerInfo.cfg";
+        File file = new File(filePath).getAbsoluteFile();
+        try {
+            FileReader fReader = new FileReader(file);
+            BufferedReader breader = new BufferedReader(new FileReader(file));
+            logger.Message("Started reading from PeerInfo");
+            while ((thisLine = breader.readLine()) != null) {
+                String[] data = thisLine.split(" ");
+                PeerInfo p = new PeerInfo();
+                p._peerId = Integer.parseInt(data[0]);
+                p._hostName = data[1];
+                p._portNumber =Integer.parseInt(data[2]);
+                p._hasFile = Boolean.parseBoolean(data[3]);
+                peers.add(p);
+            }
+
+        }
+        catch (Exception ex)
+        {
+            System.out.println(ex);
+        }
+    }
+
+}
 
